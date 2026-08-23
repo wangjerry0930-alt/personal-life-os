805f78e940e191130ec31725510d1284e0cdb43c
Kainan Wang <jerrywangboy@126.com>
2026-08-24 00:52:17 +0800

    Add daily task generation in v0.7.0

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'personal-life-os-v9';
+const CACHE = 'personal-life-os-v10';
 const BASE = self.registration.scope;
 const APP_SHELL = [BASE, `${BASE}manifest.webmanifest`, `${BASE}icons/app-icon.svg`];
 self.addEventListener('install', event => {
