724f617e61131a0803ee9dbdcf1aec1d7d54f422
Kainan Wang <jerrywangboy@126.com>
2026-08-24 00:55:34 +0800

    Add task difficulty adjustment in v0.9.0

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'personal-life-os-v11';
+const CACHE = 'personal-life-os-v12';
 const BASE = self.registration.scope;
 const APP_SHELL = [BASE, `${BASE}manifest.webmanifest`, `${BASE}icons/app-icon.svg`];
 self.addEventListener('install', event => {
