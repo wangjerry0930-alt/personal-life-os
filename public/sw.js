e94c1a1c500afa66333135a1d7a16e236cd8048e
Kainan Wang <jerrywangboy@126.com>
2026-08-24 00:09:58 +0800

    Upgrade learning and skill creation forms to v0.4.0

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'personal-life-os-v2';
+const CACHE = 'personal-life-os-v3';
 const BASE = self.registration.scope;
 const APP_SHELL = [BASE, `${BASE}manifest.webmanifest`, `${BASE}icons/app-icon.svg`];
 self.addEventListener('install', event => {
