732ce9b825d9317bba435ca44ce75ac8049a59b7
Kainan Wang <jerrywangboy@126.com>
2026-08-24 11:14:47 +0800

    Add project and goal milestones

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'personal-life-os-v39';
+const CACHE = 'personal-life-os-v41';
 const BASE = self.registration.scope;
 const APP_SHELL = [BASE, `${BASE}manifest.webmanifest`, `${BASE}icons/app-icon.svg`];
 self.addEventListener('install', event => {
