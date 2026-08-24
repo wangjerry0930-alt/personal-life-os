876ed1d19c9c420c6d07d4ee74b47c43c4d9161b
Kainan Wang <jerrywangboy@126.com>
2026-08-24 10:43:00 +0800

    Link task completion to progress

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'personal-life-os-v29';
+const CACHE = 'personal-life-os-v30';
 const BASE = self.registration.scope;
 const APP_SHELL = [BASE, `${BASE}manifest.webmanifest`, `${BASE}icons/app-icon.svg`];
 self.addEventListener('install', event => {
