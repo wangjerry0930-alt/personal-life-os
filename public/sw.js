358eb269ce66aa583dc33ddf21a0535e7a7f8415
Kainan Wang <jerrywangboy@126.com>
2026-08-24 10:46:34 +0800

    Make dashboard metrics dynamic

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'personal-life-os-v31';
+const CACHE = 'personal-life-os-v32';
 const BASE = self.registration.scope;
 const APP_SHELL = [BASE, `${BASE}manifest.webmanifest`, `${BASE}icons/app-icon.svg`];
 self.addEventListener('install', event => {
