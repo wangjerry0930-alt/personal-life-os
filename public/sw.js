13c8a03b2f7379b579ef0ecaf305510f4b7501fd
Kainan Wang <jerrywangboy@126.com>
2026-08-24 10:12:44 +0800

    Add saved knowledge library

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'personal-life-os-v23';
+const CACHE = 'personal-life-os-v24';
 const BASE = self.registration.scope;
 const APP_SHELL = [BASE, `${BASE}manifest.webmanifest`, `${BASE}icons/app-icon.svg`];
 self.addEventListener('install', event => {
