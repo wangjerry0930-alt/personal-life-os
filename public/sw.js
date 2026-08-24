cdd0cc1a93912b253969fbace85c8bf112bcea94
Kainan Wang <jerrywangboy@126.com>
2026-08-24 10:40:27 +0800

    Add journal history

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'personal-life-os-v28';
+const CACHE = 'personal-life-os-v29';
 const BASE = self.registration.scope;
 const APP_SHELL = [BASE, `${BASE}manifest.webmanifest`, `${BASE}icons/app-icon.svg`];
 self.addEventListener('install', event => {
