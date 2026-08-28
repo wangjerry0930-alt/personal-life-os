0913bd73a908bb629a45508b4cbb3df0bd864444
Kainan Wang <jerrywangboy@126.com>
2026-08-28 23:57:01 +0800

    fix: make interest priority editable

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'personal-life-os-v681';
+const CACHE = 'personal-life-os-v682';
 const BASE = self.registration.scope;
 const APP_SHELL = [BASE, `${BASE}manifest.webmanifest`, `${BASE}icons/app-icon.svg`];
 self.addEventListener('install', event => {
