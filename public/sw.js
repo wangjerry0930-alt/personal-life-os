33edb847b66bbefada1db7ec6b09373ed985964a
Kainan Wang <jerrywangboy@126.com>
2026-08-28 23:22:30 +0800

    feat: bootstrap real learning profile

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'personal-life-os-v650';
+const CACHE = 'personal-life-os-v660';
 const BASE = self.registration.scope;
 const APP_SHELL = [BASE, `${BASE}manifest.webmanifest`, `${BASE}icons/app-icon.svg`];
 self.addEventListener('install', event => {
