4ba698c63d2cee78e7db8810c5d3ed1638c38271
Kainan Wang <jerrywangboy@126.com>
2026-08-28 23:50:29 +0800

    fix: add interest archive action

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'personal-life-os-v680';
+const CACHE = 'personal-life-os-v681';
 const BASE = self.registration.scope;
 const APP_SHELL = [BASE, `${BASE}manifest.webmanifest`, `${BASE}icons/app-icon.svg`];
 self.addEventListener('install', event => {
