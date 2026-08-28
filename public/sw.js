65b7bfb7267225a75757755aec98f16f93a3f416
Kainan Wang <jerrywangboy@126.com>
2026-08-28 16:40:30 +0800

    feat: add collapsible mobile navigation

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'personal-life-os-v611';
+const CACHE = 'personal-life-os-v612';
 const BASE = self.registration.scope;
 const APP_SHELL = [BASE, `${BASE}manifest.webmanifest`, `${BASE}icons/app-icon.svg`];
 self.addEventListener('install', event => {
