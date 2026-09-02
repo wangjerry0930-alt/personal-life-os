4f7d07e715f892cb2fde33b006c5438755af83cc
Kainan Wang <jerrywangboy@126.com>
2026-09-02 20:54:43 +0800

    feat: enforce quest prerequisites

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'personal-life-os-v697';
+const CACHE = 'personal-life-os-v698';
 const BASE = self.registration.scope;
 const APP_SHELL = [BASE, `${BASE}manifest.webmanifest`, `${BASE}icons/app-icon.svg`];
 self.addEventListener('install', event => {
