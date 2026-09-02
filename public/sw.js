d959d11777093dd2ef64c6950edd7b1db98e81d4
Kainan Wang <jerrywangboy@126.com>
2026-09-02 20:47:07 +0800

    feat: add quest promotion rank progression

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'personal-life-os-v693';
+const CACHE = 'personal-life-os-v694';
 const BASE = self.registration.scope;
 const APP_SHELL = [BASE, `${BASE}manifest.webmanifest`, `${BASE}icons/app-icon.svg`];
 self.addEventListener('install', event => {
