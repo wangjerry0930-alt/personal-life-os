8d820d0de1f02569ccd3fecf1657690f7de8e9ac
Kainan Wang <jerrywangboy@126.com>
2026-09-02 21:41:11 +0800

    feat: configure daily quest count

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'personal-life-os-v6103';
+const CACHE = 'personal-life-os-v6104';
 const BASE = self.registration.scope;
 const APP_SHELL = [BASE, `${BASE}manifest.webmanifest`, `${BASE}icons/app-icon.svg`];
 self.addEventListener('install', event => {
