abce1dad95df2354a57c2e4cd106ee852556d6c7
Kainan Wang <jerrywangboy@126.com>
2026-09-02 20:50:48 +0800

    feat: require evidence for major quests

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'personal-life-os-v696';
+const CACHE = 'personal-life-os-v697';
 const BASE = self.registration.scope;
 const APP_SHELL = [BASE, `${BASE}manifest.webmanifest`, `${BASE}icons/app-icon.svg`];
 self.addEventListener('install', event => {
