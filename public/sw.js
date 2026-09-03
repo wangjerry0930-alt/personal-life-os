29314fa7a2209353da28cd09bae59157a8c66bbb
Kainan Wang <jerrywangboy@126.com>
2026-09-03 20:53:50 +0800

    feat: pin quest areas and add layout spacing

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'personal-life-os-v6123';
+const CACHE = 'personal-life-os-v6124';
 const BASE = self.registration.scope;
 const APP_SHELL = [BASE, `${BASE}manifest.webmanifest`, `${BASE}icons/app-icon.svg`];
 self.addEventListener('install', event => {
