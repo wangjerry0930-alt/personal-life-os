33d6d24a1afcdb197293141a5f87bab360a02c6c
Kainan Wang <jerrywangboy@126.com>
2026-09-03 21:50:03 +0800

    feat: sync habit toggles with daily tasks

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'personal-life-os-v6134';
+const CACHE = 'personal-life-os-v6135';
 const BASE = self.registration.scope;
 const APP_SHELL = [BASE, `${BASE}manifest.webmanifest`, `${BASE}icons/app-icon.svg`];
 self.addEventListener('install', event => {
