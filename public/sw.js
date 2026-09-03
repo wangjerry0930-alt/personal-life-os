23ae618965e5714754384b9159de593ad77efcf4
Kainan Wang <jerrywangboy@126.com>
2026-09-03 17:10:14 +0800

    feat: expand knowledge sources and period reviews

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'personal-life-os-v6108';
+const CACHE = 'personal-life-os-v6110';
 const BASE = self.registration.scope;
 const APP_SHELL = [BASE, `${BASE}manifest.webmanifest`, `${BASE}icons/app-icon.svg`];
 self.addEventListener('install', event => {
