4c0fabfa71a4fc79ddfcbbabe53b58f0bb05f8cf
Kainan Wang <jerrywangboy@126.com>
2026-09-03 16:58:10 +0800

    feat: add quest progress summary

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'personal-life-os-v6106';
+const CACHE = 'personal-life-os-v6107';
 const BASE = self.registration.scope;
 const APP_SHELL = [BASE, `${BASE}manifest.webmanifest`, `${BASE}icons/app-icon.svg`];
 self.addEventListener('install', event => {
