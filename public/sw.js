c8411520fadbd225c0b121dec7cf6177c2fa7c49
Kainan Wang <jerrywangboy@126.com>
2026-09-03 16:40:14 +0800

    feat: make quest pin controls explicit

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'personal-life-os-v6104';
+const CACHE = 'personal-life-os-v6105';
 const BASE = self.registration.scope;
 const APP_SHELL = [BASE, `${BASE}manifest.webmanifest`, `${BASE}icons/app-icon.svg`];
 self.addEventListener('install', event => {
