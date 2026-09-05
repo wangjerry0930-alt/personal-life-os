7008236f44f900e0842bdd208c1b66c6e6c2ade1
Kainan Wang <jerrywangboy@126.com>
2026-09-05 14:38:07 +0800

    feat: support epub book imports and configure pdf worker

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'personal-life-os-v6190';
+const CACHE = 'personal-life-os-v6191';
 const BASE = self.registration.scope;
 const APP_SHELL = [BASE, `${BASE}manifest.webmanifest`, `${BASE}icons/app-icon.svg`];
 self.addEventListener('install', event => {
