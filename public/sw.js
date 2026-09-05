8ea93483ec769b05fa7f2cfee0c3ba1dac13df40
Kainan Wang <jerrywangboy@126.com>
2026-09-05 16:29:39 +0800

    feat: open original PDFs in reading mode

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'personal-life-os-v6195';
+const CACHE = 'personal-life-os-v6196';
 const BASE = self.registration.scope;
 const APP_SHELL = [BASE, `${BASE}manifest.webmanifest`, `${BASE}icons/app-icon.svg`];
 self.addEventListener('install', event => {
