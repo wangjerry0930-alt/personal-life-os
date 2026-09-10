f40908544942366e251a05e74b9ad627d3651ded
Kainan Wang <jerrywangboy@126.com>
2026-09-10 17:52:50 +0800

    feat: add quest freeze protection

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'personal-life-os-v6310';
+const CACHE = 'personal-life-os-v6320';
 const BASE = self.registration.scope;
 const APP_SHELL = [BASE, `${BASE}manifest.webmanifest`, `${BASE}icons/app-icon.svg`];
 self.addEventListener('install', event => {
