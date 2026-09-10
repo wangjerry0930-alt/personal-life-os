1015c7f7612262a914448d1c5b2f7bf5b3fe0373
Kainan Wang <jerrywangboy@126.com>
2026-09-10 23:37:49 +0800

    feat: add notes to manual time sessions

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'personal-life-os-v200';
+const CACHE = 'personal-life-os-v210';
 const BASE = self.registration.scope;
 const APP_SHELL = [BASE, `${BASE}manifest.webmanifest`, `${BASE}icons/app-icon.svg`];
 self.addEventListener('install', event => {
