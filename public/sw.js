35d6bfb638ba1cca3f682e1961628b0de680e195
Kainan Wang <jerrywangboy@126.com>
2026-09-10 23:24:54 +0800

    feat: show recent time tracking history

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'personal-life-os-v170';
+const CACHE = 'personal-life-os-v180';
 const BASE = self.registration.scope;
 const APP_SHELL = [BASE, `${BASE}manifest.webmanifest`, `${BASE}icons/app-icon.svg`];
 self.addEventListener('install', event => {
