9bb62946dbb6bd8ece6ee9fc3f26fcff36fd3c56
Kainan Wang <jerrywangboy@126.com>
2026-09-10 16:58:07 +0800

    feat: add gaming allowance warnings

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'personal-life-os-v6250';
+const CACHE = 'personal-life-os-v6260';
 const BASE = self.registration.scope;
 const APP_SHELL = [BASE, `${BASE}manifest.webmanifest`, `${BASE}icons/app-icon.svg`];
 self.addEventListener('install', event => {
