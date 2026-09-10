4d4fe32de4ab3d80146813a775ff11d52a961e06
Kainan Wang <jerrywangboy@126.com>
2026-09-10 23:32:30 +0800

    feat: add inline project selector to timer

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'personal-life-os-v190';
+const CACHE = 'personal-life-os-v200';
 const BASE = self.registration.scope;
 const APP_SHELL = [BASE, `${BASE}manifest.webmanifest`, `${BASE}icons/app-icon.svg`];
 self.addEventListener('install', event => {
