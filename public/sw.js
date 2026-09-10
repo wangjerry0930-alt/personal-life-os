90d64adfaadb427e2e5771cfbe16b3fa8d359330
Kainan Wang <jerrywangboy@126.com>
2026-09-10 23:30:32 +0800

    feat: link manual timers to projects

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'personal-life-os-v180';
+const CACHE = 'personal-life-os-v190';
 const BASE = self.registration.scope;
 const APP_SHELL = [BASE, `${BASE}manifest.webmanifest`, `${BASE}icons/app-icon.svg`];
 self.addEventListener('install', event => {
