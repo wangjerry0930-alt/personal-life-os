cbd8b963e3998c5e1dd75b3cf46c1fb8eba77756
Kainan Wang <jerrywangboy@126.com>
2026-09-11 21:03:31 +0800

    test: cover time entry rollback

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'personal-life-os-v270';
+const CACHE = 'personal-life-os-v280';
 const BASE = self.registration.scope;
 const APP_SHELL = [BASE, `${BASE}manifest.webmanifest`, `${BASE}icons/app-icon.svg`];
 self.addEventListener('install', event => {
