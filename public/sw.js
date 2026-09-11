e21d85effcc857d6c43358f56c3a5304bbb6dd21
Kainan Wang <jerrywangboy@126.com>
2026-09-11 22:17:00 +0800

    feat: add generated analytics review

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'personal-life-os-v390';
+const CACHE = 'personal-life-os-v400';
 const BASE = self.registration.scope;
 const APP_SHELL = [BASE, `${BASE}manifest.webmanifest`, `${BASE}icons/app-icon.svg`];
 self.addEventListener('install', event => {
