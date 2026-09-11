125a3cb1f509cbce4f38b352a123d3a76082a0eb
Kainan Wang <jerrywangboy@126.com>
2026-09-11 21:48:24 +0800

    feat: make weekly chart keyboard accessible

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'personal-life-os-v320';
+const CACHE = 'personal-life-os-v330';
 const BASE = self.registration.scope;
 const APP_SHELL = [BASE, `${BASE}manifest.webmanifest`, `${BASE}icons/app-icon.svg`];
 self.addEventListener('install', event => {
