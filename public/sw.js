3ee3363306dec9d62038d6e87df3c3e58378c9ba
Kainan Wang <jerrywangboy@126.com>
2026-09-11 20:55:51 +0800

    feat: add live timer progress bar

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'personal-life-os-v260';
+const CACHE = 'personal-life-os-v270';
 const BASE = self.registration.scope;
 const APP_SHELL = [BASE, `${BASE}manifest.webmanifest`, `${BASE}icons/app-icon.svg`];
 self.addEventListener('install', event => {
