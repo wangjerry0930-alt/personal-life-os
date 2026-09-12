a15541736e82ab42dc24771312b807ba989874e0
Kainan Wang <jerrywangboy@126.com>
2026-09-12 18:37:06 +0800

    feat: show streak milestone progress

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'personal-life-os-v700';
+const CACHE = 'personal-life-os-v710';
 const BASE = self.registration.scope;
 const APP_SHELL = [BASE, `${BASE}manifest.webmanifest`, `${BASE}icons/app-icon.svg`];
 self.addEventListener('install', event => {
