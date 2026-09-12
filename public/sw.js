5999542b45932a8860f07c13c4f35540db50fd2e
Kainan Wang <jerrywangboy@126.com>
2026-09-12 21:30:43 +0800

    feat: sort course materials

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'personal-life-os-v880';
+const CACHE = 'personal-life-os-v890';
 const BASE = self.registration.scope;
 const APP_SHELL = [BASE, `${BASE}manifest.webmanifest`, `${BASE}icons/app-icon.svg`];
 self.addEventListener('install', event => {
