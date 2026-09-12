59ec40d986e4b0ef986cad518499234b68f8a636
Kainan Wang <jerrywangboy@126.com>
2026-09-12 16:53:06 +0800

    feat: reward course tasks by duration

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'personal-life-os-v550';
+const CACHE = 'personal-life-os-v560';
 const BASE = self.registration.scope;
 const APP_SHELL = [BASE, `${BASE}manifest.webmanifest`, `${BASE}icons/app-icon.svg`];
 self.addEventListener('install', event => {
