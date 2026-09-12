e7517141f2efa7e286e45c20a1969e1ada8c5656
Kainan Wang <jerrywangboy@126.com>
2026-09-12 20:34:04 +0800

    feat: track course material review status

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'personal-life-os-v810';
+const CACHE = 'personal-life-os-v820';
 const BASE = self.registration.scope;
 const APP_SHELL = [BASE, `${BASE}manifest.webmanifest`, `${BASE}icons/app-icon.svg`];
 self.addEventListener('install', event => {
