0425ee3840bab1b6115dab64770681075be3f1a1
Kainan Wang <jerrywangboy@126.com>
2026-09-12 22:59:25 +0800

    feat: label university course task types

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'personal-life-os-v1080';
+const CACHE = 'personal-life-os-v1090';
 const BASE = self.registration.scope;
 const APP_SHELL = [BASE, `${BASE}manifest.webmanifest`, `${BASE}icons/app-icon.svg`];
 self.addEventListener('install', event => {
