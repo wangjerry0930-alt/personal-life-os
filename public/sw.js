50b901741827f5649e3da485ac472f28707d5aa0
Kainan Wang <jerrywangboy@126.com>
2026-09-12 22:56:22 +0800

    feat: filter course tasks by type

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'personal-life-os-v1070';
+const CACHE = 'personal-life-os-v1080';
 const BASE = self.registration.scope;
 const APP_SHELL = [BASE, `${BASE}manifest.webmanifest`, `${BASE}icons/app-icon.svg`];
 self.addEventListener('install', event => {
