a7a03fab8d04be1eb8f2deb9655b1a0efaa8848c
Kainan Wang <jerrywangboy@126.com>
2026-09-12 22:49:21 +0800

    feat: start timers from upcoming course tasks

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'personal-life-os-v1050';
+const CACHE = 'personal-life-os-v1060';
 const BASE = self.registration.scope;
 const APP_SHELL = [BASE, `${BASE}manifest.webmanifest`, `${BASE}icons/app-icon.svg`];
 self.addEventListener('install', event => {
