fd224e8fce0bffd7b7032dc039e50d67708c0160
Kainan Wang <jerrywangboy@126.com>
2026-09-12 22:19:23 +0800

    feat: snooze overdue course tasks

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'personal-life-os-v1000';
+const CACHE = 'personal-life-os-v1010';
 const BASE = self.registration.scope;
 const APP_SHELL = [BASE, `${BASE}manifest.webmanifest`, `${BASE}icons/app-icon.svg`];
 self.addEventListener('install', event => {
