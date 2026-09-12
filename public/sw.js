8e4a7542bd2cc51fddcd191c034895ace8a4a9bc
Kainan Wang <jerrywangboy@126.com>
2026-09-12 20:57:01 +0800

    feat: add quick course study action

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'personal-life-os-v860';
+const CACHE = 'personal-life-os-v870';
 const BASE = self.registration.scope;
 const APP_SHELL = [BASE, `${BASE}manifest.webmanifest`, `${BASE}icons/app-icon.svg`];
 self.addEventListener('install', event => {
