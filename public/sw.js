bec8bc6413a7b9c2f84e77d4c4121b403fad33f0
Kainan Wang <jerrywangboy@126.com>
2026-09-12 23:56:43 +0800

    feat: randomize morning alarm window

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'personal-life-os-v1170';
+const CACHE = 'personal-life-os-v1180';
 const BASE = self.registration.scope;
 const APP_SHELL = [BASE, `${BASE}manifest.webmanifest`, `${BASE}icons/app-icon.svg`];
 self.addEventListener('install', event => {
