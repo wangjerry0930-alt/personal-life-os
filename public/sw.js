331558a27a7bbff7abf67ea18cb94d3957c90619
Kainan Wang <jerrywangboy@126.com>
2026-09-12 17:35:25 +0800

    feat: add spaced review course tasks

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'personal-life-os-v590';
+const CACHE = 'personal-life-os-v600';
 const BASE = self.registration.scope;
 const APP_SHELL = [BASE, `${BASE}manifest.webmanifest`, `${BASE}icons/app-icon.svg`];
 self.addEventListener('install', event => {
