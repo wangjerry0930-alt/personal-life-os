c7e24a45803871a8ab34c13c7a349225a0f85bd8
Kainan Wang <jerrywangboy@126.com>
2026-09-12 21:48:38 +0800

    feat: link course planning to today

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'personal-life-os-v920';
+const CACHE = 'personal-life-os-v930';
 const BASE = self.registration.scope;
 const APP_SHELL = [BASE, `${BASE}manifest.webmanifest`, `${BASE}icons/app-icon.svg`];
 self.addEventListener('install', event => {
