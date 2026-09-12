1ac6a33903e8a4a9f1c3af45beb24577e640370e
Kainan Wang <jerrywangboy@126.com>
2026-09-12 16:49:47 +0800

    feat: add university course task templates

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'personal-life-os-v540';
+const CACHE = 'personal-life-os-v550';
 const BASE = self.registration.scope;
 const APP_SHELL = [BASE, `${BASE}manifest.webmanifest`, `${BASE}icons/app-icon.svg`];
 self.addEventListener('install', event => {
