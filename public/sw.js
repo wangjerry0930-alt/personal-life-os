1063cceeb31470808bf3484a4ffda32fe11421d2
Kainan Wang <jerrywangboy@126.com>
2026-09-12 21:37:44 +0800

    feat: include course notes in course packs

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'personal-life-os-v900';
+const CACHE = 'personal-life-os-v910';
 const BASE = self.registration.scope;
 const APP_SHELL = [BASE, `${BASE}manifest.webmanifest`, `${BASE}icons/app-icon.svg`];
 self.addEventListener('install', event => {
