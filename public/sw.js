0169a0ac9969a0f01f8f189d82f3d084e5431966
Kainan Wang <jerrywangboy@126.com>
2026-09-13 11:13:38 +0800

    feat: choose task time category

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'personal-life-os-v1250';
+const CACHE = 'personal-life-os-v1260';
 const BASE = self.registration.scope;
 const APP_SHELL = [BASE, `${BASE}manifest.webmanifest`, `${BASE}icons/app-icon.svg`];
 self.addEventListener('install', event => {
