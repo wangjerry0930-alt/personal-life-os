d2bda5a22cecbf2873a5c055ce21e195b95dc9bb
Kainan Wang <jerrywangboy@126.com>
2026-09-13 10:45:58 +0800

    fix: canonicalize language quest domains

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'personal-life-os-v1220';
+const CACHE = 'personal-life-os-v1230';
 const BASE = self.registration.scope;
 const APP_SHELL = [BASE, `${BASE}manifest.webmanifest`, `${BASE}icons/app-icon.svg`];
 self.addEventListener('install', event => {
