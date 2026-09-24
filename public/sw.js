8b975bba5c57904ad7f2118dcef435ae4b19944e
wangjerry0930-alt <wangjerry0930@gmail.com>
2026-09-24 15:33:05 +0100

    feat: add backdated time entries in v17.4

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE='personal-life-os-v1730';
+const CACHE='personal-life-os-v1740';
 const BASE=self.registration.scope;
 const APP_SHELL=[BASE,`${BASE}manifest.webmanifest`,`${BASE}icons/app-icon.svg`];
 
